a47b6f3998e229fcc8ad6f5f23bf7782c387e4b6
pironatto <pironatto@gmail.com>
2024-07-16 22:53:13 -0300

    Implementação de Controle de Tempo

--- Assets/Scripts/BancoDados.cs
+++ Assets/Scripts/BancoDados.cs
@@ -1,8 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+
 
 
 
@@ -10,12 +12,19 @@ public class BancoDados : MonoBehaviour
 {
 
     private string[] itens;     //ARMAZENAR OS ITENS
-    public Text pergunta, r1, r2, r3, r4;
+    public TextMeshProUGUI r1, r2, r3, r4;
+    public Text pergunta;
+    public Button R1, R2, R3, R4;
+    private controleTempo _controleTempo;
+    [HideInInspector]
+    public bool clickBotao;
 
 
     // Start is called before the first frame update
     IEnumerator Start()
     {
+
+        _controleTempo = FindObjectOfType(typeof(controleTempo)) as controleTempo;
 
         //ESPERAR ATÉ QUE O BANCO DE DADOS SEJA LIDO
         using (UnityWebRequest itemdata = UnityWebRequest.Get("https://zeleystudios.servegame.com/corujao/perguntas.php"))
@@ -30,18 +39,50 @@ public class BancoDados : MonoBehaviour
             r2.text = itens[4];
             r3.text = itens[5];
             r4.text = itens[6];
-
+            print(itens[7]);
 
         }
-
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        verificaResposta();
+    }
+
+
+    void verificaResposta()
+    {
+        if (_controleTempo.tempoEsgotado == true || clickBotao == true)
+        {
+            switch (itens[7])
+            {
+                case "A":
+                    R1.image.color = Color.red;
+                    break;
+                case "B":
+                    R2.image.color = Color.red;
+                    break;
+                case "C":
+                    R3.image.color = Color.red;
+                    break;
+                case "D":
+                    R4.image.color = Color.red;
+                    break;
+            }
+        }
 
     }
+
+    public void BotaoResposta()
+    {
+        string botaoClicado = EventSystem.current.currentSelectedGameObject.name;
+        clickBotao = true;
+
+    }
+
+
 
 
 
---
+++ Assets/Scripts/controleTempo.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+public class controleTempo : MonoBehaviour
+{
+
+    private BancoDados _bancoDados;
+    private float currentTime, currentTimeSlider;
+    public Slider BarraProgresso, SliderA, SliderB;
+    public TextMeshProUGUI Tempo;
+    [HideInInspector]
+    public bool tempoEsgotado = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _bancoDados = FindObjectOfType(typeof(BancoDados)) as BancoDados;
+
+        //ACIONAR BARRA DE TEMPO
+        currentTime = 10;
+        currentTimeSlider = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BarraLoad();
+
+        if (tempoEsgotado == false)
+        {
+            SliderLoad();
+        }
+
+        if (BarraProgresso.value == 0 || _bancoDados.clickBotao == true)
+        {
+            tempoEsgotado = true;
+            StartCoroutine("ChamarFade");
+
+        }
+
+    }
+
+    public void BarraLoad()
+    {
+        currentTime -= Time.deltaTime;
+        BarraProgresso.value = currentTime;
+        Tempo.text = Mathf.RoundToInt(BarraProgresso.value).ToString();
+
+    }
+
+
+    public void SliderLoad()
+    {
+        currentTimeSlider += Time.deltaTime;
+        SliderA.value = currentTimeSlider;
+        SliderB.value = currentTimeSlider;
+
+    }
+
+    IEnumerator ChamarFade()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(3);
+
+
+    }
+
+}
